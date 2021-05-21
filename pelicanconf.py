1ac1fed7472393dbecbae9bd68581d8711201fc5
antoine.sireyjol@gmail.com <antoine.sireyjol@gmail.com>
2021-05-21 17:33:33 +0200

    test changement siteurl et sitemap

--- pelicanconf.py
+++ pelicanconf.py
@@ -55,19 +55,7 @@ COLOR_SCHEME_CSS = 'github.css'
 # PLUGIN_PATHS = ['pelican-plugins']
 # PLUGINS = ['pelican-toc']
 
-SITEMAP = {
-    "format": "xml",
-    "priorities": {
-        "articles": 0.5,
-        "indexes": 0.5,
-        "pages": 0.5
-    },
-    "changefreqs": {
-        "articles": "monthly",
-        "indexes": "daily",
-        "pages": "monthly"
-    }
-}
+
 
 
 
--- publishconf.py
+++ publishconf.py
@@ -10,13 +10,27 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'https://statoscop.github.io/blog'
+SITEURL = 'https://blog.statoscop.fr'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
 
 DELETE_OUTPUT_DIRECTORY = True
+
+SITEMAP = {
+    "format": "xml",
+    "priorities": {
+        "articles": 0.5,
+        "indexes": 0.5,
+        "pages": 0.5
+    },
+    "changefreqs": {
+        "articles": "monthly",
+        "indexes": "daily",
+        "pages": "monthly"
+    }
+}
 
 # Following items are often useful when publishing
 
